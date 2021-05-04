a075a08170532f8585d456463ca7945c6f7e5930
gabvrel <gabvrel@gmail.com>
2021-05-04 18:32:42 -0500

    loading

---
+++ next.config.js
@@ -0,0 +1,4 @@
+react: {
+    useSuspense: false;
+    wait: true
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,73 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
-import FirstSection from '../componets/FirstSection';
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import FirstSection from "../componets/FirstSection";
 import SecondSection from "../componets/SecondSection";
 import ThirdSection from "../componets/ThirdSection";
 import FourthSection from "../componets/FourthSection";
 import FifthSection from "../componets/FifthSection";
 import { gsap, Expo } from "gsap";
-import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
 
-const Home = () => { 
+const Home = () => {
+  const [isLoaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
   return (
     <>
-        <Head>
-          <title>Feniks</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>Feniks</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {!isLoaded ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            flexDirection: "column",
+          }}
+        >
+          <h1>Loading...</h1>
+        </div>
+      ) : (
         <main className={styles.main}>
-          <section id='firstSec'>
-            <FirstSection/>
+          <section id="firstSec">
+            <FirstSection />
           </section>
-          <section id='secondSec'>
-            <SecondSection/>
+          <section id="secondSec">
+            <SecondSection />
           </section>
-          <section id='thirdSec'>
+          <section id="thirdSec">
             <ThirdSection />
           </section>
-          <section id='fourthSec'>
+          <section id="fourthSec">
             <FourthSection />
           </section>
-          <section id='fifthSec'>
+          <section id="fifthSec">
             <FifthSection />
           </section>
         </main>
-        <footer className={styles.footer}>
-            <h5>© 2021 Copyright. All rights reserved.</h5>
-            <h5>Quito-Ecuador, Sector Norte</h5>
-            <h5>Designed with love by <a href='#'><img src='assets/minelogo.png' /></a> </h5>
-        </footer>
+      )}
+      <footer className={styles.footer}>
+        <h5>© 2021 Copyright. All rights reserved.</h5>
+        <h5>Quito-Ecuador, Sector Norte</h5>
+        <h5>
+          Designed with love by{" "}
+          <a href="#">
+            <img src="assets/minelogo.png" />
+          </a>{" "}
+        </h5>
+      </footer>
     </>
-  )
-}
-
+  );
+};
 
 export default Home;
